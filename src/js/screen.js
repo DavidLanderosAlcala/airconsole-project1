e8e94d3667317fc9d1e895b905996fe8a2afa1f8
David Landeros <david.landeros@gameloft.com>
2017-01-06 10:17:39 -0800

    Added a logo; added a subtitle label; updated docs

--- src/js/screen.js
+++ src/js/screen.js
@@ -4,23 +4,56 @@
   */
 var Screen = (function(){
 
+    var canvas;
+    var context;
+
     /** @func init
       * @desc Called when the DOM is loaded
       */
     function init()
     {
-        
+        canvas = document.getElementById("main_canvas");
+        context = canvas.getContext("2d");
+    }
+
+    /** @func setLogoVisibility
+      * @desc show/hide the logo using a CSS3 transition
+      * @param visible {boolean} send true for show the logo or false to hide it
+      */
+    function setLogoVisibility(visible)
+    {
+        document.getElementById("main_logo")
+        .style.opacity = visible ? 1.0 : 0.0;
+    }
+
+    /** @func setSubtitleText
+      * @desc It sets the subtitles
+      * @param text {string} the text to be shown
+      */
+    function setSubtitleText(text)
+    {
+        document.getElementById("subtitles_label")
+        .innerHTML = text;
     }
 
     /** @func render
-      * @desc It renders the scene
+      * @desc It renders the scene, called from Screen.update
       */
     function render()
     {
 
     }
 
-    return { init   : init,
-             render : render };
+    /** @func updated
+      * @desc called before each frame to update the scene
+      */
+    function update()
+    {
+        render();
+    }
+
+    return { init : init,
+             setLogoVisibility : setLogoVisibility,
+             setSubtitleText : setSubtitleText };
 
 })();
