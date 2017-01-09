dfedf394ae1081c6f665c4fc76fa4ecc9327047c
David Landeros <david.landeros@gameloft.com>
2017-01-09 10:45:48 -0800

    Updated Sprite module (WIP)

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -22,7 +22,7 @@ var Sprite = (function(){
    /**
      * @func createSprite
      * @desc creates a new sprite-based animation
-     * @param {object} options object containing, filename, width, height, fps [, x_offset, y_offset]
+     * @param {object} options object containing, filename, width, height, fps [, x_offset, y_offset, starting_frame]
      * @param {function} callback function to be called when the resource are complete loaded
      * @return {object} returns an instance of the inner class Sprite
      */
@@ -37,13 +37,29 @@ var Sprite = (function(){
      */
    function SpriteClass(options, callback)
    {
+        this.callback = callback == undefined ? function(){ } : callback;
         this.filename = options.filename;
         this.width = options.width;
         this.height = options.height;
         this.fps = options.fps == undefined ? 15 : options.fps;
         this.x_offset = options.x_offset == undefined ? 0 : options.x_offset;
         this.y_offset = options.y_offset == undefined ? 0 : options.y_offset;
-        this.callback = callback == undefined ? function(){ } : callback;
+        this.starting_frame = options.starting_frame == undefined ? 0 : options.starting_frame;
+        this.current_frame = this.starting_frame;
+        this.time_limit = 1 / this.fps * 1000;
+        this.current_time = new Date().getTime();
+        this.time_stamp = 0;
+   }
+
+   /**
+     * @func reset
+     * @desc It resets the Sprite (time, and starting frame)
+     */
+   SpriteClass.prototype.reset = function()
+   {
+        this.current_frame = this.starting_frame;
+        this.time_stamp = 0;
+        this.current_time = new Date().getTime();
    }
 
 
