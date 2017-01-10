a0030a831a2632956d2437d426c3607560b0d810
David Landeros <david.landeros@gameloft.com>
2017-01-10 15:19:52 -0800

    Finished Sprite class ?

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -5,7 +5,7 @@ class Sprite {
    /**
      * @class Sprite
      * @desc creates a new sprite-based animation
-     * @param {object} options object containing, filename, width, height, fps [, x_offset, y_offset, starting_frame]
+     * @param {object} options object containing: filename, width, height, frame_count, fps [, x_offset, y_offset, starting_frame]
      * @param {function} callback function to be called when the resource are complete loaded
      */
    constructor(options, callback)
@@ -19,9 +19,18 @@ class Sprite {
         this.y_offset = options.y_offset == undefined ? 0 : options.y_offset;
         this.starting_frame = options.starting_frame == undefined ? 0 : options.starting_frame;
         this.current_frame = this.starting_frame;
+        this.frame_count = options.frame_count;
         this.time_limit = 1 / this.fps * 1000;
         this.current_time = new Date().getTime();
         this.time_stamp = 0;
+        this.img = new Image();
+        this.img.src = this.filename;
+        this.img.addEventListener("onload", function() {
+            callback();
+        });
+        this.img.addEventListener("onerror", function() {
+            callback({ message : "couldn't load the image" });
+        });
    }
 
     /**
@@ -57,11 +66,45 @@ class Sprite {
      * @instance
      * @param {number} x Coordinates 
      * @param {number} y Coordinates 
+     * @param {number} w width (optional)
+     * @param {number} h height (optional)
      * @desc Draws the sprite's current frame on the canvas
      */
-   render(x, y)
+   render(x, y, w, h)
    {
+        this.calcCurrentFrame();
+        var sx = (this.current_frame * this.width) + this.x_offset;
+        var sy = this.y_offset;
+        var sw = this.width;
+        var sh = this.height;
+        var dx = x;
+        var dy = y;
+        var dw = w == undefined ? this.width : w;
+        var dh = h == undefined ? this.height : h;
+        Sprite.context.drawImage(this.img, sx, sy, sw, sh, dx, dy, dw, dh);
+   }
 
+   /**
+     * @method calcCurrentFrame
+     * @memberof Sprite
+     * @instance
+     * @desc Sets the current frame based on the time elapsed since the last call
+     */
+   calcCurrentFrame()
+   {
+        var new_time = new Date().getTime();
+        this.time_stamp += new_time - this.current_time;
+        this.current_time = new_time;
+
+        if(this.time_stamp > this.time_limit)
+        {
+            this.time_stamp = 0;
+            this.current_frame++;
+            if(this.current_frame == this.frame_count)
+            {
+                this.current_frame = 0;
+            }
+        }
    }
 
 }
