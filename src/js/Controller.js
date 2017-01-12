db2b529402de9ba459d9cf1e4c31d9a9a0fa8c87
David Landeros <dh.landeros08@gmail.com>
2017-01-11 22:16:11 -0800

    setTimeout is back because airconsole.onReady sucks a BBC >:(

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -13,15 +13,21 @@ var Controller = (function(){
     {
         airconsole = new AirConsole();
         DebugConsole.init(airconsole, false);
-        
-        airconsole.onConnect = function() {
-            DebugConsole.log("Hola mundo");
+
+        airconsole.onConnect = function(device_id) {
+            DebugConsole.log("Hola mundo Connected");
         };
+
+        airconsole.onReady = function(code) {
+            DebugConsole.log("Hola mundo Ready");
+        };
+
+        setTimeout(function(){
+            DebugConsole.log("Hola mundo setTimeout");
+        }, 1000);
     }
 
     return { init : init };
 
 })();
 Controller.init();
-
-
