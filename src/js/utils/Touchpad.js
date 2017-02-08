954fa523c0ba8d60f91e2b1173b9f69a44a4ae21
David Landeros <david.landeros@gameloft.com>
2017-02-08 14:59:44 -0800

    Added touchpad module

---
+++ src/js/utils/Touchpad.js
@@ -0,0 +1,47 @@
+
+/**
+  * @Module Touchpad
+  */
+var Touchpad = (function(){
+
+    var last_pos = null;
+    var onmove_callback = null;
+    
+    /** @func surface
+      * @desc converts DOM elements into touchpad and installs an event listener
+      * @param selector {string} the CSS selector to hook the DOM elements
+      * @param callback {function} called for each event ocurred in the touchpad
+      */    
+    function init(selector, callback)
+    {
+        onmove_callback = callback;
+        Touch.surface(selector, onTouchEvent);
+    }
+    
+    function onTouchEvent(e)
+    {
+        if(e.type == "touchend")
+        {
+            last_pos = null;
+            return;
+        }
+        if(last_pos == null)
+        {
+            last_pos = {
+                x : e.x,
+                y : e.y
+            };
+        }
+        else
+        {
+            var disp = {
+               x : (e.x - last_pos.x),
+               y : (e.y - last_pos.y)
+            };
+            onmove_callback(disp);
+            last_pos.x = e.x;
+            last_pos.y = e.y;
+        }
+    }
+    return { init : init };
+})();
