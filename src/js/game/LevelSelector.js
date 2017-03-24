9d5ef9a20f6212aca2300fba07d50b607004db98
David Landeros <david.landeros@gameloft.com>
2017-03-23 17:19:07 -0700

    Enter fullscreen on level selected

--- src/js/game/LevelSelector.js
+++ src/js/game/LevelSelector.js
@@ -38,6 +38,7 @@ var LevelSelector = (function(){
     	{
             hide();
             CrayonPhysics.loadLevel(parseInt(e.sender.innerHTML));
+            Screen.requestFullscreen();
     	}
     }
 
