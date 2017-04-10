19cd696cd1ea3517132f5f5794a2de6118084d1c
David Landeros <david.landeros@gameloft.com>
2017-04-10 10:37:24 -0700

    Add level 6

--- src/res/levels/level_6.js
+++ src/res/levels/level_6.js
@@ -2,63 +2,102 @@
 LevelSelector.getLevels().push({
 
     title       : "Level 0",
-    description : "???????",
+    description : "Despata la coladera",
     bodies      : [
         {
-            label : "ground",
+            label : "plattformA",
             isStatic : true,
-            position : { x : 0, y : 100 },
+            position : { x : -500, y : 0 },
             vertices: [
-                {x : -600, y : 0 },
-                {x : -600, y : 100 },
-                {x : 600,  y : 100 },
-                {x : 600,  y : 0 }
-            ],
-        },
-        {
-            label : "rock",
-            isStatic : false,
-            position : { x : 0, y : 200 },
-            vertices: [
-                {x : -50, y : 0 },
-                {x : -50, y : 100 },
-                {x : 50,  y : 100 },
-                {x : 50,  y : 0 }
-            ],
-        },
-        {
-            label : "rock",
-            isStatic : false,
-            position : { x : 0, y : 300 },
-            vertices: [
-                {x : -50, y : 0 },
-                {x : -50, y : 100 },
-                {x : 50,  y : 100 },
-                {x : 50,  y : 0 }
-            ],
-        },
-        {
-            label : "rock",
-            isStatic : false,
-            position : { x : 0, y : 400 },
-            vertices: [
-                {x : -50, y : 0 },
-                {x : -50, y : 100 },
-                {x : 50,  y : 100 },
-                {x : 50,  y : 0 }
+                {x : -800, y : 800 },
+                {x : -800, y : 0 },
+                {x : 400, y : 0 },
+                {x : 350, y : 200 },
             ],
         },
 
         {
-            label : "platform",
+            label : "plattformA",
             isStatic : true,
-            position : { x : 0, y : 600 },
+            position : { x : 380, y : 0 },
             vertices: [
-                {x : -80, y : 0 },
-                {x : -80, y : 50 },
-                {x : 80,  y : 50 },
-                {x : 80,  y : 0 }
+                {x : 100, y : 400 },
+                {x : 100, y : 800 },
+                {x : 150, y : 800 },
+                {x : 150, y : 0 },
+                {x : 100, y : 0 },
+                {x : -400, y : 0 },
+                {x : -350, y : 200 },
             ],
+        },
+
+        {
+            label : "rock",
+            isStatic : false,
+            position : { x : -50, y : 400 },
+            vertices: [
+                {x : -60, y  :  90 },
+                {x : -60, y  : -90 },
+                {x :  60, y  : -90 },
+                {x :  60, y  :  90 },
+            ],
+        },
+
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -430, y : 500 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -380, y : 500 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -400, y : 600 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -400, y : 400 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -500, y : 500 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : -400, y : 550 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : 300, y : 550 },
+        },
+        {
+            label    : "ball",
+            type     : "circle",
+            radio    : 25,
+            isStatic : false,
+            position : { x : 300, y : 500 },
         },
     ],
 
