467808d982a05ed074f9f39f6d5fe5e55a40f616
David Landeros <david.landeros@gameloft.com>
2017-04-12 13:55:42 -0700

    Update level 8 (WIP)

--- src/res/levels/level_8.js
+++ src/res/levels/level_8.js
@@ -2,24 +2,148 @@
 LevelSelector.getLevels().push({
 
     title       : "Level 7",
-    description : "cinta transportadora",
+    description : "Sube la caja al pickup",
     bodies : [
         {
             label : "lane",
             isStatic : true,
-            position : { x : 0, y : 1 },
+            position : { x : -3, y : 1 },
             vertices: [ { x: -4, y: 0 },
                         { x: -4, y: 1 },
                         { x: 4, y: 1 },
                         { x: 4, y: 0 } ],
         },
+        {
+            label : "wheel",
+            isStatic : true,
+            type : "circle",
+            position : { x : 4.7, y : 2.5 },
+            radio : 0.4,
+        },
+        {
+            label : "wheel",
+            isStatic : true,
+            type : "circle",
+            position : { x : 8.7, y : 2.5 },
+            radio : 0.4,
+        },
+        {
+            label : "box",
+            isStatic : false,
+            position : { x : -8, y : 10 },
+            vertices: [
+                {x : -0.30, y : 0.30 },
+                {x : -0.30, y : -0.30 },
+                {x : 0.30, y : -0.30 },
+                {x : 0.30, y : 0.30 },
+            ],
+        },
+        {
+            label : "base",
+            isStatic : true,
+            position : { x : -10, y : 5 },
+            vertices : [
+                { "x" : 2.9, "y": 0 },
+                { "x" : 2.9, "y": 0.5 },
+                { "x" : 3.7, "y": 0.5 },
+                { "x" : 3.7, "y": -0.8 },
+                { "x" : -2.7, "y": -0.8 },
+                { "x" : -2.7, "y": 0 },
+                { "x" : -2.7, "y": 1.8 },
+                { "x" : 0, "y": 1.8 },
+                { "x" : 0, "y": 0 }
+            ]
+        },
+        {
+            label : "sensor",
+            isStatic : true,
+            isSensor : true,
+            position : { x : 4.7, y : 4 },
+            vertices: [
+                {x : -1.5, y :  0.50 },
+                {x : -1.5, y : -0.50 },
+                {x :  1.5, y : -0.50 },
+                {x :  1.5, y :  0.50 },
+            ],
+        },
+        {
+            label : "pickup",
+            isStatic : true,
+            position : { x : 6, y : 3 },
+            vertices: [
+                { "x": -3.2, "y": 0.6 },
+                { "x": -3.2, "y": -0.4 },
+                { "x": -1.8, "y": -0.4 },
+                { "x": -1.8, "y": -0.2 },
+                { "x": -1.6, "y": 0 },
+                { "x": -1.4, "y": 0 },
+                { "x": -1.2, "y": 0 },
+                { "x": -0.9, "y": -0.1 },
+                { "x": -0.8, "y": -0.4 },
+                { "x": 2.4, "y": -0.4 },
+                { "x": 2.4, "y": -0.2 },
+                { "x": 2.5, "y": 0 },
+                { "x": 2.8, "y": 0 },
+                { "x": 3, "y": 0 },
+                { "x": 3.2, "y": -0.2 },
+                { "x": 3.2, "y": -0.4 },
+                { "x": 4.2, "y": -0.4 },
+                { "x": 4.2, "y": 0.1 },
+                { "x": 4.2, "y": 0.4 },
+                { "x": 3.9, "y": 0.6 },
+                { "x": 2.5, "y": 0.6 },
+                { "x": 2.3, "y": 1 },
+                { "x": 2, "y": 1.4 },
+                { "x": 1.6, "y": 1.6 },
+                { "x": 0, "y": 1.6 },
+                { "x": 0, "y": 0.6 }
+            ],
+        },
+    ],
+
+    hints: [
+        {
+            opacity : 1.0,
+            position : { x : -3 , y : 1.5 },
+            vertices : [
+                { "x": -0.79, "y": 0 },
+                { "x": 0.74, "y": 0 },
+                { "x": 0.36, "y": 0.38 },
+                { "x": 0.74, "y": 0 },
+                { "x": 0.36, "y": -0.38 },
+                { "x": 0.74, "y": 0 }
+            ],
+        },
+        {
+            position : { x : 7.4 , y : 3.6 },
+            opacity : 1.0,
+            vertices : [
+                { "x": -0.8, "y": 0.8 },
+                { "x": -0.8, "y": 0 },
+                { "x": 0.8, "y": 0 },
+                { "x": 0, "y": 0.8 },
+                { "x": -0.8, "y": 0.8 }
+            ]
+        }
     ],
 
     setup : function(context)
     {
         /* creamos un array en el objeto context
            el cual contiene los objetos que estan tocando la cinta transportadora */
+        context.gameover = false;
         context.objects_over_lane = [];
+
+        var bodies = Physics.getAllBodies();
+        for(var i = 0; i < bodies.length; i++)
+        {
+            if(Physics.getLabel(bodies[i]) == "box")
+            {
+                context.box = bodies[i];
+                context.box_start_position = Physics.getPosition(context.box);
+                break;
+            }
+        }
 
 	    /* cuando INICIA una colision, revisamos si la cinta transportadora esta involucrada */
         Physics.on("beginContact", function(event){
@@ -32,6 +156,13 @@ LevelSelector.getLevels().push({
             {
                 /* Metemos el objeto al arreglo */
                 context.objects_over_lane.push(event.bodyA);
+            }
+            if( Physics.getLabel(event.bodyA) == "sensor" || Physics.getLabel(event.bodyB) == "sensor" )
+            {
+                if( Physics.getLabel(event.bodyA) == "box" || Physics.getLabel(event.bodyB) == "box" )
+                {
+                    context.gameover = true;
+                }
             }
         });
 
@@ -70,7 +201,15 @@ LevelSelector.getLevels().push({
             /* Se re-aplica en cada frame porque velocidad suele desaparecer por causa de la friccion */
             Physics.setVelocity(context.objects_over_lane[i], {x: 100, y: 0});
         }
-        return false;
+
+        if( Physics.getPosition(context.box).y >= Screen.getHeight())
+        {
+            // la regresamos a su posicion inicial
+            Physics.setPosition(context.box, context.box_start_position );
+            Physics.clearForces(context.box);
+            context.time = new Date().getTime();
+        }
+        return context.gameover;
     }
 
 });
