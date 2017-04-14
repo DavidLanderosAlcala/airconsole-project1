e5f4fbf63d568782df43c23fd5315c93203a2a50
David Landeros <david.landeros@gameloft.com>
2017-04-13 17:09:29 -0700

    Re-enable polygon correction

--- src/js/game/CrayonPhysics.js
+++ src/js/game/CrayonPhysics.js
@@ -395,31 +395,32 @@ var CrayonPhysics = (function(){
       }
 
       var poly = Utils.matterToP2Flavor(drawing_data.current_polygon);
-      if(!decomp.isSimple(poly))
-      {
-          return "wire";
-      }
-      //var removed_vertices = 0;
-      //var isSimple = false;
-      //while(!isSimple && removed_vertices < 20)
-      //{
-      //    isSimple = decomp.isSimple(poly);
-      //    if(!isSimple)
-      //    {
-      //            poly.splice(poly.length -1 , 1);
-      //            poly.splice(0, 1);
-      //            removed_vertices+=2;
-      //    }
-      //}
-      //drawing_data.current_polygon = Utils.p2ToMatterFlavor(poly);
-      //if(!isSimple)
+      //if(!decomp.isSimple(poly))
       //{
       //    return "wire";
       //}
-      //if(removed_vertices > 0)
-      //{
-      //    console.log("removed_vertices: " + removed_vertices);
-      //}
+
+      var removed_vertices = 0;
+      var isSimple = false;
+      while(!isSimple && removed_vertices < 10)
+      {
+          isSimple = decomp.isSimple(poly);
+          if(!isSimple)
+          {
+                  poly.splice(poly.length -1 , 1);
+                  poly.splice(0, 1);
+                  removed_vertices+=2;
+          }
+      }
+      drawing_data.current_polygon = Utils.p2ToMatterFlavor(poly);
+      if(!isSimple)
+      {
+          return "wire";
+      }
+      if(removed_vertices > 0)
+      {
+          console.log("removed_vertices: " + removed_vertices);
+      }
 
       return "polygon";
   }
