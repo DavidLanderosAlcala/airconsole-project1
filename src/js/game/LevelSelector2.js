928dd0adc185b5464e34f739f5f55b50e37a1b11
David Landeros <david.landeros@gameloft.com>
2017-05-16 12:19:06 -0700

    Add new level selector (experimental; WIP)

---
+++ src/js/game/LevelSelector2.js
@@ -0,0 +1,116 @@
+
+/*
+     Game.loadLevel(level_index);
+*/
+
+/**
+  * @module LevelSelector
+  */
+var LevelSelector = (function(){
+
+	var levels = [];
+	var level_metadata = [];
+	var container = null;
+	var listeners = [];
+    var visible = false;
+
+	function init()
+	{
+        var levelsHTML = "";
+        for(var i = 0; i < levels.length; i++)
+        {
+        	levelsHTML += GenLevelLauncherHTML(i);
+        }
+        container = document.createElement("div");
+        container.id = "level-selector-container";
+        document.body.appendChild(container);
+        container.innerHTML = levelsHTML;
+
+        on("preview", function(level_index){
+        	Screen.setTitleText(levels[level_index].title);
+        });
+
+        on("selected", function(level_index){
+           hide();
+           Game.loadLevel(level_index);
+        });
+	}
+
+	function show()
+	{
+		 Screen.setTitleText("Level selector");
+		 visible = true;
+         container.style.zIndex = 11;
+         container.style.opacity = 1;
+         container.style.pointerEvents = 'auto'; 
+	}
+
+	function hide()
+	{
+		visible = false;
+        container.style.zIndex = -11;
+        container.style.opacity = 0;
+        container.style.pointerEvents = 'none';
+	}
+
+	function getLevels()
+	{
+		return levels;
+	}
+
+	function GenLevelLauncherHTML(level_index)
+	{
+		var filled1 = "false";
+		var filled2 = "false";
+		var filled3 = "false";
+		var unlocked = "true";
+		if(level_index > 0)
+			unlocked = "false";
+        var html =  '<div onmouseover="LevelSelector.dispatch(\'preview\',' + level_index + ')" ' +
+                    ' onclick="LevelSelector.dispatch(\'selected\',' + level_index + ')" class="level-launcher" data-unlocked="' + unlocked + '">\r\n' +
+            	    '	<div class="level-number"> ' + (level_index + 1) + ' </div>\r\n' +
+            	    '	<div class="level-stars-container">\r\n' +
+            	    '		<span class="level-star" data-filled="' + filled1 + '"></span>\r\n' +
+            	    '		<span class="level-star" data-filled="' + filled2 + '"></span>\r\n' +
+            	    '		<span class="level-star" data-filled="' + filled3 + '"></span>\r\n' +
+            	    '	</div>\r\n' +
+            	    '</div>\r\n';
+        return html;
+	}
+
+     /** @func isVisible
+       */
+    function isVisible()
+    {
+        return visible;
+    }
+
+    function on(type, callback)
+    {
+        if(!listeners[type])
+        {
+            listeners[type] = [];
+        }
+        listeners[type].push(callback);
+    }
+
+    function dispatch(event, arg)
+    {
+        if(listeners[event])
+        {
+            for(var i = 0; i < listeners[event].length; i++)
+            {
+                listeners[event][i](arg);
+            }
+        }
+    }
+
+    return { init        : init,
+             getLevels   : getLevels,
+             show        : show,
+             hide        : hide,
+             isVisible   : isVisible,
+             on          : on,
+             dispatch    : dispatch
+    };
+})();
